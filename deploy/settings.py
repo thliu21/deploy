580fe5c236489075396bb864396dd0d1f10e5d9c
Tianhao Liu <liux3271@umn.edu>
2017-04-01 15:28:33 -0500

    update

--- deploy/settings.py
+++ deploy/settings.py
@@ -79,10 +79,9 @@ WSGI_APPLICATION = 'deploy.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
